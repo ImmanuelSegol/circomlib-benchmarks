bc6cbc03eda340f4442138ba3a514782950abad3
ImmanuelSegol <3ditds@gmail.com>
2022-12-12 01:13:54 +0000

    refactor

--- benchmarks/bench_poseidonex.sh
+++ benchmarks/bench_poseidonex.sh
@@ -1,4 +1,1 @@
 sh prep_circute.sh poseidonex_test.circom > poseidon_groth16.log
-
-# todo: clean up
-# rm poseidonex_test_benchmark_files
--- benchmarks/prep_circute.sh
+++ benchmarks/prep_circute.sh
@@ -6,38 +6,36 @@ PTAU_FILE=../ptau/powersOfTau28_hez_final_14.ptau
 # ADD FILES FOLDER TO GITIGNORE
 # ADD C++ FOLDER TO GITIGNORE
 
+node_modules=`dirname \`pwd\``
 circuit_name=${1%.*}
 root="$circuit_name"_benchmark_files
 circom_source_code=../test/circuits/$1
 
-if ! -d "./$root"
-then
-    mkdir $root
-    echo $root
+mkdir $root
+echo $root
 
-    echo "~~~COMPILING CIRCUIT~~~"
-    circom "$circom_source_code" --r1cs --sym --c --output ./"$root"
-    cd $root
+echo "~~~COMPILING CIRCUIT~~~"
+circom "$circom_source_code" --r1cs --sym --c --output ./"$root"
+cd $root
 
-    echo "~~~GENERATING WITNESS FOR~~~"
-    set -x
-    cd "$circuit_name"_cpp 
-    make 
-    ./"$circuit_name" ../../inputs/bench_poseidon.json ../"$circuit_name"_witness.wtns > ../log.out
-    cd ..
+echo "~~~GENERATING WITNESS FOR~~~"
+set -x
+cd "$circuit_name"_cpp 
+make 
+./"$circuit_name" ../../inputs/bench_poseidon.json ../"$circuit_name"_witness.wtns > ../log.out
+cd ..
 
-    echo "~~~GENERATING ZKEY 0~~~"
-    node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc /home/ubuntu/monia/benchmark/platforms/node_modules/snarkjs/cli.js zkey new ./"$circuit_name".r1cs "$PTAU_FILE" ./"$circuit_name"_0.zkey
+echo "~~~GENERATING ZKEY 0~~~"
+node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc "$node_modules"/node_modules/snarkjs/cli.js zkey new ./"$circuit_name".r1cs "$PTAU_FILE" ./"$circuit_name"_0.zkey
 
-    echo "~~~GENERATING FINAL ZKEY~~~"
-    node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc /home/ubuntu/monia/benchmark/platforms/node_modules/snarkjs/cli.js zkey beacon ./"$circuit_name"_0.zkey ./"$circuit_name".zkey 0102030405060708090a0b0c0d0e0f101112231415161718221a1b1c1d1e1f 10 -n="Final Beacon phase2"
+echo "~~~GENERATING FINAL ZKEY~~~"
+node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc "$node_modules"/node_modules/snarkjs/cli.js zkey beacon ./"$circuit_name"_0.zkey ./"$circuit_name".zkey 0102030405060708090a0b0c0d0e0f101112231415161718221a1b1c1d1e1f 10 -n="Final Beacon phase2"
 
-    echo "~~~VERIFYING FINAL ZKEY~~~"
-    node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc /home/ubuntu/monia/benchmark/platforms/node_modules/snarkjs/cli.js zkey verify ./"$circuit_name".r1cs "$PTAU_FILE" ./"$circuit_name".zkey
+echo "~~~VERIFYING FINAL ZKEY~~~"
+node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc "$node_modules"/node_modules/snarkjs/cli.js zkey verify ./"$circuit_name".r1cs "$PTAU_FILE" ./"$circuit_name".zkey
 
-    echo "~~~EXPORTING VERIFICATION KEY~~~"
-    node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc /home/ubuntu/monia/benchmark/platforms/node_modules/snarkjs/cli.js zkey export verificationkey ./"$circuit_name".zkey ./"$circuit_name"_vkey.json
-fi
+echo "~~~EXPORTING VERIFICATION KEY~~~"
+node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc "$node_modules"/node_modules/snarkjs/cli.js zkey export verificationkey ./"$circuit_name".zkey ./"$circuit_name"_vkey.json
 
 echo "~~~GENERATING PROOF FOR SAMPLE INPUT~~~"
 start=$(date +%s%N)
